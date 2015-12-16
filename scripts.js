f36db4f225300e7deab0bf9c138e2fd7fc462945
dominik59 <osemka59@gmail.com>
2015-12-16 16:30:25 +0100

    dodany kalendarz i dodawanie nowego lekarza

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 //////////////////////////strona główna
 $( document ).ready(function() {
 	$('#button1').on('click',function(){
-			var login = $('#login_id').val();
-			$.ajax({ 
-			  type:"POST",                                     
+		var login = $('#login_id').val();
+		$.ajax({ 
+		  type:"POST",                                     
 	      url: 'singing_in.php',                  //the script to call to get data          
 	      data: {login},
 	                              //you can insert url argumnets here to pass to api.php
@@ -20,9 +20,13 @@ $( document ).ready(function() {
 	      		{
 	      			$.mobile.changePage( $("#admin_page"), "slide", true, true);
 	      		}
-	      		if(perm=="user")
+	      		if(perm=="nurse")
 	      		{
 	      			$.mobile.changePage($('#nurse_page'),"slide",true,true);
+	      		}
+	      		if(perm=="doctor")
+	      		{
+	      			$.mobile.changePage($('#add_new_doctor_page'),"slide",true,true);
 	      		}
 	      	}
 	        
@@ -31,18 +35,9 @@ $( document ).ready(function() {
 
 
 
-			/*if($.trim(login) != '') //trim usuwa spacje
-			{
-				$.post('user_name.php',{login : login},function(data){
-						alert(data[0]);
-						if($("#password_id").val()==data)
-						{
-							$.mobile.changePage( $("#page2"), "slide", true, true);
-						}
-						//w tym miejscu następuje rurchomienie pliku name.php i przekazanie mu zmiennej która nazywać się będzie name a wartość jej to będzie to co siedzi w zmienej tymczasowej name stworzonej wcześniej. to co zostanie zwrócone to data,które jest argumentem funkcji
-				});
-			}*/
-		});
+				
+	});
+
     $( "#flip_switch_id" ).on( 'slidestop', function() {
     	
     	if($("#flip_switch_id").val()=="Tak")
@@ -57,15 +52,16 @@ $( document ).ready(function() {
     		
     	}
     });
-$("#flip_switch_div_id").hide();
-$("#button1").prop("disabled",true);
-$("#button1").prop("disabled",false);
-$("#password_id").bind("tap",function (){
-	$("#flip_switch_div_id").show(500);
-});
-$("#login_id").bind("tap",function(){
-	$("#flip_switch_div_id").hide(500);
-});
+
+	$("#flip_switch_div_id").hide();
+	$("#button1").prop("disabled",true);
+	$("#button1").prop("disabled",false);
+	$("#password_id").bind("tap",function (){
+		$("#flip_switch_div_id").show(500);
+	});
+	$("#login_id").bind("tap",function(){
+		$("#flip_switch_div_id").hide(500);
+	});
 });
 //////////////////////////////strona główna
 
@@ -78,4 +74,10 @@ $("#login_id").bind("tap",function(){
 //////////////////////////////nurse_page
 
 //////////////////////////////nurse_page
+
+
+//////////////////////////////add_new_doctor_page
+
+//////////////////////////////add_new_doctor_page
+
 
